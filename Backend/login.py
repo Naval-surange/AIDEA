d7cb5b3bee8ca49a734a9ded52df69a21f7515fd
Naval-surange <naval.s@research.iiit.ac.in>
2022-04-20 00:15:41 +0530

    Fixed import errors

--- Backend/login.py
+++ Backend/login.py
@@ -1,7 +1,6 @@
 import sqlite3
 import hashlib
 import streamlit as st
-import pandas as pd
 from Session import get_session_id
 from hydralit import HydraHeadApp
 
@@ -20,7 +19,7 @@ def check_hashes(password, hashed_text):
 
 
 # DB Management
-conn = sqlite3.connect('data.db',check_same_thread=False)
+conn = sqlite3.connect('./Backend/data.db',check_same_thread=False)
 c = conn.cursor()
 # DB  Functions
 
@@ -69,7 +68,7 @@ class Login(HydraHeadApp):
 
 class Signup(HydraHeadApp):
 	def run(self):
-		st.subheader("Create New Account")
+		st.title("Create New Account")
 		new_user = st.text_input("Username")
 		new_password = st.text_input("Password",type='password')
 
@@ -81,65 +80,3 @@ class Signup(HydraHeadApp):
 
 
 
-
-
-
-# def login():
-# 	"""Simple Login App"""
-# 	global logged_in
-# 	st.title("Login")
-# 	# menu = ["Home","Login","SignUp"]
-# 	# choice = st.selectbox("Menu",menu)
-
-
-# 	if not logged_in:
-# 		st.subheader("Login Section")
-
-# 		username = st.text_input("User Name")
-# 		password = st.text_input("Password",type='password')
-# 		login_btn = st.button("Login")
-# 		signup_btn = st.button("SignUp")
-
-# 		if login_btn:
-# 			# if password == '12345':
-# 			create_usertable()
-# 			hashed_pswd = make_hashes(password)
-
-# 			result = login_user(username,check_hashes(password,hashed_pswd))
-# 			if result:
-# 				logged_in = True
-# 				# st.success("Logged In as {}".format(username))
-
-# 				# task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
-# 				# if task == "hashed_pswd = make_hashesAdd Post":
-# 				# 	st.subheader("Add Your Post")
-
-# 				# elif task == "Analytics":
-# 				# 	st.subheader("Analytics")
-# 				# elif task == "Profiles":
-# 				# 	st.subheader("User Profiles")
-# 				# 	user_result = view_all_users()
-# 				# 	clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
-# 				# 	st.dataframe(clean_db)
-# 			else:
-# 				st.warning("Incorrect Username/Password")
-
-
-
-
-
-# 	elif signup_btn:
-# 		st.subheader("Create New Account")
-# 		new_user = st.text_input("Username")
-# 		new_password = st.text_input("Password",type='password')
-
-# 		if st.button("Signup"):
-# 			create_usertable()
-# 			add_userdata(new_user,make_hashes(new_password))
-# 			st.success("You have successfully created a valid Account")
-# 			st.info("Go to Login Menu to login")
-
-
-
-# if __name__ == '__main__':
-# 	login()
--- dashboard.py
+++ dashboard.py
@@ -1,25 +1,36 @@
-import streamlit as st
-st.set_page_config(layout="wide",page_title='AIDEA', page_icon="💡")
-
 import sys
-sys.path.insert(1,"./Frontend")
-sys.path.insert(1,"./Backend")
+sys.path.insert(1, "./Frontend")
+sys.path.insert(1, "./Backend")
 
-from PIL import Image
-import Frontend.classif_dashboard as classif_dashboard
-import Frontend.regress_dashboard as regress_dashboard
-import Frontend.ts_dashboard as ts_dashboard
 
-from hydralit import HydraApp
-from Backend.login import Login, Signup
-from Backend.Session import get_session_id
 import warnings
-warnings.filterwarnings('always')  # "error", "ignore", "always", "default", "module" or "once"
+from Backend.Session import get_session_id
+from Backend.login import Login, Signup
+import hydralit_components as hc
+from hydralit import HydraApp
+import Frontend.ts_dashboard as ts_dashboard
+import Frontend.regress_dashboard as regress_dashboard
+import Frontend.classif_dashboard as classif_dashboard
+from PIL import Image
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title='AIDEA', page_icon="💡")
+
+hide_streamlit_style = """
+                    <style>
+                    footer {visibility: hidden;}
+                    </style>
+                    """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+
+# "error", "ignore", "always", "default", "module" or "once"
+warnings.filterwarnings('always')
 
 logo = Image.open('./Frontend/logo.png')
 
-st.image(logo,width=300,clamp=True)
-st.markdown("<b>AI</b> <b>D</b>evelopment, <b>E</b>xplainability, and <b>A</b>bstraction  \n<small><i>Minimalistic No-Code platform to convert your AI Ideas into Reality</i></small>",unsafe_allow_html=True)
+st.image(logo, width=300, clamp=True)
 
 
 login_app = Login()
@@ -27,14 +38,20 @@ signup_app = Signup()
 
 logged_in = False
 
+over_theme = {'txc_inactive': 'White', 'menu_background': 'black'}
+
 
 placeholder = st.empty()
 with placeholder.container():
     if(not logged_in):
-        app = HydraApp()
-        app.add_app("Login",app=login_app)
-        app.add_app("Signup",app=signup_app)
+        app = HydraApp(navbar_theme=over_theme)
+
+        app.add_app("Login", app=login_app, icon="🔒")
+        app.add_app("Signup", app=signup_app, icon="🔐")
         app.run()
+        st.markdown("---")
+        st.markdown("<small><center><b>A Ripik.ai Product</b></center></small>",
+                unsafe_allow_html=True)
 
 fp = open("./Backend/logged_in_user.txt", "r")
 for line in fp:
@@ -68,10 +85,12 @@ if(logged_in):
         st.write("1. Select Use Case.   ")
         st.write("2. Choose Dataset from Sidebar.")
         st.write("3. Fill Required Information.")
-        st.markdown("4. <i>Run Experiment</i> and Train your model within minutes <i>(Feature Engineering & Training will be done automatically)</i>.",unsafe_allow_html=True)
-        st.markdown("5. Review performance metrics and explainability through <i>XAI Dashboard.</i>",unsafe_allow_html=True)
+        st.markdown("4. <i>Run Experiment</i> and Train your model within minutes <i>(Feature Engineering & Training will be done automatically)</i>.", unsafe_allow_html=True)
+        st.markdown(
+            "5. Review performance metrics and explainability through <i>XAI Dashboard.</i>", unsafe_allow_html=True)
         st.markdown("---")
-        st.markdown("<small><center><i>Note: Trained Models should not be used in production using current version of AIDEA.</i></center></small>",unsafe_allow_html=True)
+        st.markdown("<small><center><i>Note: Trained Models should not be used in production using current version of AIDEA.</i></center></small>", unsafe_allow_html=True)
 
     st.sidebar.markdown("---")
-    st.markdown("<small><center><b>A Ripik.ai Product</b></center></small>",unsafe_allow_html=True)
+    st.markdown("<small><center><b>A Ripik.ai Product</b></center></small>",
+                unsafe_allow_html=True)
